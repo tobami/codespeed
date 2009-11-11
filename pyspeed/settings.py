425efe3a1176b475dd06a5963c9d12e8ef86e01e
Miquel Torres <tobami@googlemail.com>
2009-11-11 11:35:28 +0000

    make media path relative

--- pyspeed/settings.py
+++ pyspeed/settings.py
@@ -5,6 +5,8 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+BASEDIR = os.path.abspath( os.path.dirname(__file__).replace('\\','/') )
+
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
@@ -37,7 +39,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/miquel/code/codespeed/pyspeed/media'
+MEDIA_ROOT = BASEDIR + "/media/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).

59fba9cd4d1fccb626744e56b03df84edc687d28
Miquel Torres <tobami@googlemail.com>
2009-11-16 00:14:35 +0000

    Added some comments

--- tools/save_result.py
+++ tools/save_result.py
@@ -2,21 +2,21 @@
 from datetime import datetime
 import urllib, urllib2
 
-BASEURL = 'http://localhost:8080/'
+BASEURL = 'http://localhost:8080/'# This will be pyspeed.pypy.org/
 
 def add_result():
     data = {
         'revision_number': '23238',
         'revision_project': 'pypy',
-        'revision_date': "2009-11-15 18:11:29", # Optional
+        'revision_date': "2009-11-15 18:11:29", # Optional. Make mandatory?
         'interpreter_name': 'pypy-c-jit',
         'interpreter_coptions': 'gc=Boehm',
         'benchmark_name': 'Richards',
-        'benchmark_type': 'P',# Optional. Default Trunk. (Trunk, Debug, Python, Multilanguage)
+        'benchmark_type': 'P',# Optional. Default is T for Trunk. (Trunk, Debug, Python, Multilanguage)
         'environment': "Dual Core Linux",
         'result_key': 'total',
         'result_value': 400,
-        'result_type': 'M',# Optional. Default Time in milliseconds. (Time, Memory, Score)
+        'result_type': 'M',# Optional. Default is 'T' for Time in milliseconds. (Time, Memory, Score)
         'result_date': datetime.today(),
     }
     

6b576eca58e4f38d86eae21082541eea53793f53
Miquel Torres <tobami@googlemail.com>
2010-03-15 15:15:31 +0100

    Add home and about page

--- speedcenter/codespeed/urls.py
+++ speedcenter/codespeed/urls.py
@@ -1,31 +1,11 @@
     # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail
 from django.views.generic.simple import direct_to_template, redirect_to
-from codespeed.models import Result, Revision, Interpreter
 import settings
 
-def getresults():
-    #Function so that results are not cached
-    return Result.objects.filter(revision__project=settings.PROJECT_NAME)
-
-def getinterpreters():
-    #Function so that interpreters are not cached
-    return Interpreter.objects.filter(name__startswith=settings.PROJECT_NAME)
-
-revision_list = {
-    'queryset': Revision.objects.filter(project=settings.PROJECT_NAME).order_by('-date').filter(),
-    'template_name': 'revision_list.html',
-    'template_object_name': 'revision',
-    'extra_context': {
-        'result_list': getresults(),
-        'interpreter_list': getinterpreters(),
-    }
-}
-
 urlpatterns = patterns('',
-    #(r'^$', direct_to_template, {'template': 'base.html'}),
-    (r'^$', redirect_to, {'url': '/overview'}),
+    (r'^$', direct_to_template, {'template': 'home.html'}),
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
 )
 
 urlpatterns += patterns('codespeed.views',
@@ -33,9 +13,6 @@ urlpatterns += patterns('codespeed.views',
     (r'^overview/table/$', 'getoverviewtable'),
     (r'^timeline/$', 'timeline'),
     (r'^timeline/json/$', 'gettimelinedata'),
-    #(r'^comparison/$', 'comparison'),
-    #(r'^results/$', 'results'),
-    #(r'^results/table/$', 'resultstable'),
 )
 
 urlpatterns += patterns('codespeed.views',

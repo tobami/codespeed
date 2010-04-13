72acaccca37a029a77d577eb91a4ff47a3bc0941
Miquel Torres <tobami@googlemail.com>
2010-04-13 15:44:45 +0200

    Remove settings options

--- speedcenter/codespeed/settings.py
+++ speedcenter/codespeed/settings.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
-defaultbaseline = {'executable': 4, 'revision': 262}
-defaultexecutable = 1
+## Define some default options here ##
+#defaultbaseline = {'executable': 4, 'revision': 262}
+#defaultexecutable = 1

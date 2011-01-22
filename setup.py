ebdbcb6ca84eb513bc3cb62e269d6a9d74ff614d
Chris Adams <chris@improbable.org>
2011-01-22 17:41:09 -0500

    Add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Miquel Torres',
+    author_email='tobami@gmail.com',
+    name='codespeed',
+    version='0.7.0',
+    url='https://github.com/tobami/codespeed',
+    license='GNU Lesser General Public License version 2.1',
+    requires=['django'],
+    packages=find_packages(exclude=['ez_setup']),
+    description='A web application to monitor and analyze the performance of your code',
+    include_package_data=True,
+    zip_safe=False,
+)

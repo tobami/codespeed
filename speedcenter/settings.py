1db5499d3a17ee1c176e945c9f72f1c7bd8df54c
Stefan Marr <git@stefan-marr.de>
2011-02-20 23:03:18 +0100

    Added logging for unhandled exceptions in debug mode.

--- speedcenter/settings.py
+++ speedcenter/settings.py
@@ -70,6 +70,23 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
+if DEBUG:
+    import traceback
+    import logging
+    
+    # Define a class that logs unhandled errors
+    class LogUncatchedErrors:
+        def process_exception(self, request, exception):
+            logging.error("Unhandled Exception on request for %s\n%s" %
+                                 (request.build_absolute_uri(),
+                                  traceback.format_exc()))
+    # And add it to the middleware classes
+    MIDDLEWARE_CLASSES += ('settings.LogUncatchedErrors',)
+    
+    # set shown level of logging output to debug
+    logging.basicConfig(level=logging.DEBUG)
+
+
 ROOT_URLCONF = 'speedcenter.urls'
 
 TEMPLATE_DIRS = (

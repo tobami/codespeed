35c60cb542dc77c7b1edfffc6e53a1867591d815
daniloaf <daniloaf@ubuntu.(none)>
2011-03-28 00:43:52 -0300

    - Changing timelines to plot only branches results (still plots only one branch per plot)

--- speedcenter/codespeed/views.py
+++ speedcenter/codespeed/views.py
@@ -327,66 +327,69 @@ def gettimelinedata(request):
         exeid, revid = data['base'].split("+")
         baselinerev = Revision.objects.get(id=revid)
         baselineexe = Executable.objects.get(id=exeid)
-    for bench in benchmarks:
-        append = False
-        lessisbetter = bench.lessisbetter and ' (less is better)' or ' (more is better)'
-        timeline = {
-            'benchmark':             bench.name,
-            'benchmark_id':          bench.id,
-            'benchmark_description': bench.description,
-            'units':                 bench.units,
-            'lessisbetter':          lessisbetter,
-            'executables':           {},
-            'baseline':              "None",
-        }
-
-        for executable in executables:
-            resultquery = Result.objects.filter(
-                    benchmark=bench
-                ).filter(
-                    environment=environment
-                ).filter(
-                    executable=executable
-                ).select_related(
-                    "revision"
-                ).order_by('-revision__date')[:number_of_revs]
-            print resultquery
-            if not len(resultquery):
-                continue
-
-            results = []
-            for res in resultquery:
-                std_dev = ""
-                if res.std_dev != None:
-                    std_dev = res.std_dev
-                results.append(
-                    [str(res.revision.date), res.value, std_dev, res.revision.get_short_commitid()]
-                )
-            timeline['executables'][executable] = results
-            append = True
-        if baselinerev != None and append:
-            try:
-                baselinevalue = Result.objects.get(
-                    executable=baselineexe,
-                    benchmark=bench,
-                    revision=baselinerev,
-                    environment=environment
-                ).value
-            except Result.DoesNotExist:
-                timeline['baseline'] = "None"
-            else:
-                # determine start and end revision (x axis) from longest data series
+    branches = {}
+    for branch in data.get('branch_list', '').split(','):
+        for bench in benchmarks:
+            append = False
+            lessisbetter = bench.lessisbetter and ' (less is better)' or ' (more is better)'
+            timeline = {
+                'benchmark':             bench.name,
+                'benchmark_id':          bench.id,
+                'benchmark_description': bench.description,
+                'units':                 bench.units,
+                'lessisbetter':          lessisbetter,
+                'executables':           {},
+                'baseline':              "None",
+            }
+            for executable in executables:
+                resultquery = Result.objects.filter(
+                        benchmark=bench
+                    ).filter(
+                        environment=environment
+                    ).filter(
+                        executable=executable
+                    ).filter(
+                        revision__branch=branch
+                    ).select_related(
+                        "revision"
+                    ).order_by('-revision__date')[:number_of_revs]
+                if not len(resultquery):
+                    continue
+    
                 results = []
-                for exe in timeline['executables']:
-                    if len(timeline['executables'][exe]) > len(results):
-                        results = timeline['executables'][exe]
-                end = results[0][0]
-                start = results[len(results)-1][0]
-                timeline['baseline'] = [
-                    [str(start), baselinevalue],
-                    [str(end), baselinevalue]
-                ]
-        if append: timeline_list['timelines'].append(timeline)
+                for res in resultquery:
+                    std_dev = ""
+                    if res.std_dev != None:
+                        std_dev = res.std_dev
+                    results.append(
+                        [str(res.revision.date), res.value, std_dev, res.revision.get_short_commitid()]
+                    )
+                timeline['executables'][executable] = results
+                append = True
+            if baselinerev != None and append:
+                try:
+                    baselinevalue = Result.objects.get(
+                        executable=baselineexe,
+                        benchmark=bench,
+                        revision=baselinerev,
+                        environment=environment
+                    ).value
+                except Result.DoesNotExist:
+                    timeline['baseline'] = "None"
+                else:
+                    # determine start and end revision (x axis) from longest data series
+                    results = []
+                    for exe in timeline['executables']:
+                        if len(timeline['executables'][exe]) > len(results):
+                            results = timeline['executables'][exe]
+                    end = results[0][0]
+                    start = results[len(results)-1][0]
+                    timeline['baseline'] = [
+                        [str(start), baselinevalue],
+                        [str(end), baselinevalue]
+                    ]
+            if append: branches[branch] = timeline
+        if len(branches): timeline_list['timelines'].append({'branches': branches})
 
     if not len(timeline_list['timelines']):
         response = 'No data found for the selected options'

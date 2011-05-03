5a4ad000f2f0bedf9ec606dcaf389ad0832284da
Frank Becker <fb@alien8.de>
2011-05-03 11:49:12 +0200

    Now any path in url should work
    Added example to urls.py how to add paths to
    URL, e. g. http://host/speed/ and described it
    in the README

--- example/urls.py
+++ example/urls.py
@@ -4,6 +4,8 @@ import os.path
 
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, handler404, handler500
+from django.views.generic.simple import redirect_to
+from django.core.urlresolvers import reverse
 from django.contrib import admin
 
 
@@ -13,17 +15,22 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
 
+urlpatterns += patterns(
+    '',
+    #('^$', redirect_to, {'url': '/speed/'}),
+)
+
+urlpatterns += patterns(
+    '',
+    (r'^', include('codespeed.urls')),
+    #(r'^speed/', include('codespeed.urls')),
+)
+
 if settings.DEBUG or settings.SERVE_STATIC:
     urlpatterns += patterns('',
-        (r'^media/override/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.OVERRIDE_MEDIA_ROOT}),
         (r'^media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT}),
         (r'^admin_media/(?P<path>.*)$', 'django.views.static.serve',
             {'document_root': os.path.join(os.path.dirname(admin.__file__),
                                             'media')}),
     )
-
-urlpatterns += patterns('',
-    (r'^', include('codespeed.urls')),
-)

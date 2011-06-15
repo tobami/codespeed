a3d696e002c47439433793e7bf7c319a8b382586
Frank Becker <fb@alien8.de>
2011-06-15 10:53:57 +0200

    removed template overwrite

--- example/settings.py
+++ example/settings.py
@@ -42,7 +42,6 @@ USE_I18N = False
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(BASEDIR, "media")
-OVERRIDE_MEDIA_ROOT = os.path.join(BASEDIR, "..", "override", "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -90,7 +89,6 @@ if DEBUG:
 ROOT_URLCONF = 'example.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), '..', 'override', 'templates'),
     os.path.join(os.path.dirname(__file__), 'templates'),
 )
 

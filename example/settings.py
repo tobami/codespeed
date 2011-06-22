6936a0e8fde2dc2d1e9dee9f3aa248fba00e7f2f
Frank Becker <fb@alien8.de>
2011-06-22 14:27:12 +0200

    Added docu for deployment w/ nginx+gunicorn or apache2+mod-wsgi
      * Fixed admin static files to be served by staticfiles
      * Added docu and according files to set up nginx and gunicorn
      * Added docu and according files to set up apache2 using mod-wsgi as a vhost

--- example/settings.py
+++ example/settings.py
@@ -51,7 +51,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin_media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'as%n_m#)^vee2pe91^^@c))sl7^c6t-9r8n)_69%)2yt+(la2&'

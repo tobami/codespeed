570ee38345b494c76ac41ef163527c15e5ecc9f7
Frank Becker <fb@alien8.de>
2011-06-23 11:44:30 +0200

    Merged setup.py, bumped version number, fixed requirements
        * Merged from master
        * updated version string to 0.8
        * now require Djano 1.3 and South

--- setup.py
+++ setup.py
@@ -1,30 +1,15 @@
-import os
 from setuptools import setup, find_packages
-
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
-readme = f.read()
-f.close()
 
 setup(
     author='Miquel Torres',
     author_email='tobami@gmail.com',
     name='codespeed',
-    version='0.7.0',
+    version='0.8.0',
     url='https://github.com/tobami/codespeed',
     license='GNU Lesser General Public License version 2.1',
-    requires=['django', 'isodate'],
-    packages=find_packages(exclude=['ez_setup', 'speedcenter']),
+    install_requires=['django>=1.3', 'isodate', 'south'],
+    packages=find_packages(exclude=['ez_setup']),
     description='A web application to monitor and analyze the performance of your code',
-    long_description=readme,
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License version 2.1',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-    ],
 )

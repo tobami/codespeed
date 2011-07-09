7385947fb61f17b707029f3b17198230a98c0caf
Stefan Marr <git@stefan-marr.de>
2011-07-09 18:48:45 +0200

    Fixed issues with the value for equidistant plotting
    - the settings was not correctly preserved with permalinks and navigation
    - looks like jQuery Address does not like plain boolean values
    - permalinks are currently based on support from the server, added that, since it was missing
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- codespeed/views.py
+++ codespeed/views.py
@@ -521,6 +521,11 @@ def timeline(request):
             defaultbenchmark = data['ben']
         else:
             defaultbenchmark = get_object_or_404(Benchmark, name=data['ben'])
+    
+    if 'equid' in data:
+        defaultequid = data['equid']
+    else:
+        defaultequid = "off"
 
     # Information for template
     executables = Executable.objects.filter(project__track=True)
@@ -536,7 +541,8 @@ def timeline(request):
         'benchmarks': benchmarks,
         'environments': enviros,
         'branch_list': branch_list,
-        'defaultbranch': defaultbranch
+        'defaultbranch': defaultbranch,
+        'defaultequid' : defaultequid
     }, context_instance=RequestContext(request))
 
 

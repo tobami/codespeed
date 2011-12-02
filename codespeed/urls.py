c56a12a0700d8e61fa0105cb7ba396459555b6eb
Frank Becker <fb@alien8.de>
2011-12-02 23:53:27 +0100

    started resource for benchmark results

--- codespeed/urls.py
+++ codespeed/urls.py
@@ -4,13 +4,14 @@ from django.core.urlresolvers import reverse
 from django.views.generic.simple import direct_to_template
 from tastypie.api import Api
 from codespeed.feeds import LatestEntries
-from codespeed.api import UserResource, EnvironmentResource
+from codespeed.api import UserResource, EnvironmentResource, BenchmarkResultResource
 
 feeds = { 'latest': LatestEntries }
 
 rest_api = Api(api_name='v1')
 rest_api.register(EnvironmentResource())
 rest_api.register(UserResource())
+rest_api.register(BenchmarkResultResource())
 
 urlpatterns = patterns('',
     (r'^$', direct_to_template, {'template': 'home.html'}),

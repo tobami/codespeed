aac1273c3f0bde281ceca856ee7df85edc4f4030
Frank Becker <fb@alien8.de>
2011-12-03 23:45:25 +0100

    on the way to BenchmarkResultResource, obj_get() first working implementation
        - While reading tastypie docu and try-and-error got obj_get()
          implemented. Yeah! Probably, it's all rubbish

--- codespeed/api.py
+++ codespeed/api.py
@@ -23,6 +23,7 @@ DELETE Environment() data:
 
 See http://django-tastypie.readthedocs.org/en/latest/interacting.html
 """
+import logging
 
 from django.contrib.auth.models import User
 from django.db import models
@@ -126,7 +127,9 @@ class ReportResource(ModelResource):
 
 
 class BenchmarkResultResource(Resource):
-    """Ressource for all the data of a benchmark result
+    """Ressource for all the data of a benchmark result.
+
+       Primarily used to submit benchmark results
 
         mandatory data
         'commitid',
@@ -151,3 +154,63 @@ class BenchmarkResultResource(Resource):
         authorization= Authorization()
         allowed_methods = ['get', 'post', 'put', 'delete']
 
+    def get_object_list(self, request):
+        query = self._client().add('messages')
+        query.map("function(v) { var data = JSON.parse(v.values[0].data); return [[v.key, data]]; }")
+        results = []
+
+        for result in query.run():
+            new_obj = RiakObject(initial=result[1])
+            new_obj.uuid = result[0]
+            results.append(new_obj)
+
+        return results
+
+    def obj_get_list(self, request=None, **kwargs):
+        """Return all benchmark results ever
+        """
+        return self.get_object_list(request)
+
+    def obj_get(self, request=None, **kwargs):
+        pk = kwargs['pk']
+        #logging.debug(kwargs)
+        d =    {
+            'xbenchmark': 2,
+            'xrevision': 1,
+            'branch': 1,
+            'xexecutable': 1,
+            'xenvironment': 1,
+            'result': 1,
+            'project': 1,
+            }
+        result = Result.objects.get(pk=pk)
+        result.project = result.executable.project
+        result.result = result
+        result.branch = result.revision
+        return result
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        bundle.obj = RiakObject(initial=kwargs)
+        bundle = self.full_hydrate(bundle)
+        bucket = self._bucket()
+        new_message = bucket.new(bundle.obj.uuid, data=bundle.obj.to_dict())
+        new_message.store()
+        return bundle
+
+    def obj_update(self, bundle, request=None, **kwargs):
+        return self.obj_create(bundle, request, **kwargs)
+
+    def obj_delete_list(self, request=None, **kwargs):
+        bucket = self._bucket()
+
+        for key in bucket.get_keys():
+            obj = bucket.get(key)
+            obj.delete()
+
+    def obj_delete(self, request=None, **kwargs):
+        bucket = self._bucket()
+        obj = bucket.get(kwargs['pk'])
+        obj.delete()
+
+    def rollback(self, bundles):
+        pass
--- codespeed/urls.py
+++ codespeed/urls.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.views.generic.simple import direct_to_template
 from tastypie.api import Api
 from codespeed.feeds import LatestEntries
-from codespeed.api import UserResource, EnvironmentResource, BenchmarkResultResource, ProjectResource
+from codespeed.api import UserResource, EnvironmentResource, BenchmarkResultResource, ProjectResource, ExecutableResource, BenchmarkResource, ResultResource, BranchResource, RevisionResource
 
 feeds = { 'latest': LatestEntries }
 
@@ -12,6 +12,11 @@ rest_api = Api(api_name='v1')
 rest_api.register(EnvironmentResource())
 rest_api.register(UserResource())
 rest_api.register(ProjectResource())
+rest_api.register(ExecutableResource())
+rest_api.register(BenchmarkResource())
+rest_api.register(ResultResource())
+rest_api.register(BranchResource())
+rest_api.register(RevisionResource())
 rest_api.register(BenchmarkResultResource())
 
 urlpatterns = patterns('',

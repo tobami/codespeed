51d4b63439e883ca2c69e2d7962f3593b573e16d
Noah Kantrowitz <noah@coderanger.net>
2013-01-06 12:19:33 -0800

    exec wrapper script.

---
+++ manage.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import os
+import sys
+
+new_file = os.path.join(os.path.dirname(__file__), 'example', 'manage.py')
+os.execl(sys.executable, sys.executable, new_file, *sys.argv)

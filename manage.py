29dec697781fd8a9a14bb9c3c7603976a45d1afc
Noah Kantrowitz <noah@coderanger.net>
2013-01-06 12:32:53 -0800

    No really, I'm bad at unix.

--- manage.py
+++ manage.py
@@ -5,5 +5,4 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_project.settings")
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)

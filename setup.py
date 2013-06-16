a12ebcb55e49261944083b5b483c6ccbd4acd2b8
Miquel Torres <tobami@gmail.com>
2013-06-16 10:10:22 +0200

    Pin Django dependency to 1.4 Fixes #135 Avoid future South version breakage

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.9.1',
     url='https://github.com/tobami/codespeed',
     license='GNU Lesser General Public License version 2.1',
-    install_requires=['django>=1.3', 'isodate', 'south', 'django-tastypie'],
+    install_requires=['django==1.4', 'isodate', 'south<=2.0', 'django-tastypie'],
     packages=find_packages(exclude=['ez_setup', 'sample_project']),
     description='A web application to monitor and analyze the performance of your code',
     include_package_data=True,

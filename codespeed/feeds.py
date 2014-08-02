9014aa6f87b02e28c7c84f2ee57a2dcf050eca7f
Joachim Breitner <mail@joachim-breitner.de>
2014-08-02 16:25:12 +0200

    Refactor feed generation a bit

--- codespeed/feeds.py
+++ codespeed/feeds.py
@@ -1,25 +1,26 @@
 from django.contrib.syndication.views import Feed
 from codespeed.models import Report
 from django.conf import settings
+from django.db.models import Q
 
-
-class LatestEntries(Feed):
+class ResultFeed(Feed):
     title = settings.WEBSITE_NAME
     link = "/changes/"
+
+    def items(self):
+        return Report.objects\
+            .filter(self.result_filter())\
+            .order_by('-revision__date')[:10]
+
+class LatestEntries(ResultFeed):
     description = "Last benchmark runs"
 
-    def items(self):
-        return Report.objects.filter(
-            revision__branch__name=settings.DEF_BRANCH
-        ).order_by('-revision__date')[:10]
+    def result_filter(self):
+        return Q(revision__branch__name=settings.DEF_BRANCH)
 
-class LatestSignificantEntries(Feed):
-    title = settings.WEBSITE_NAME
-    link = "/changes/"
+class LatestSignificantEntries(ResultFeed):
     description = "Last significant benchmark runs"
 
-    def items(self):
-        return Report.objects.filter(
-            revision__branch__name=settings.DEF_BRANCH,
-            colorcode__in = ('red','green')
-        ).order_by('-revision__date')[:10]
+    def result_filter(self):
+        return Q(revision__branch__name=settings.DEF_BRANCH,
+                 colorcode__in = ('red','green'))

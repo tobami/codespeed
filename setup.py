285299cdb8afd8ad8617118da0e87627faf601a2
Miquel Torres <tobami@gmail.com>
2016-05-24 10:19:07 +0200

    Fix setup.py to allow django 1.8, like requirements.txt

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='codespeed',
-    version='0.11.0a0',
+    version='0.11.0a1',
     author='Miquel Torres',
     author_email='tobami@gmail.com',
     url='https://github.com/tobami/codespeed',
     download_url="https://github.com/tobami/codespeed/tags",
     license='GNU Lesser General Public License version 2.1',
     keywords=["benchmarking", "visualization"],
-    install_requires=['django>=1.4,<1.8', 'isodate>=0.4.7,<0.6'],
+    install_requires=['django>=1.6,<1.9', 'isodate>=0.4.7,<0.6'],
     packages=find_packages(exclude=['ez_setup', 'sample_project']),
     description='A web application to monitor and analyze the performance of your code',
     include_package_data=True,
@@ -22,10 +22,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
     ]
 )

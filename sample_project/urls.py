cce53b9d65d43a78010cdb271ccb218a431ac44e
Miquel Torres <tobami@gmail.com>
2017-08-16 13:15:10 +0400

    Django 1.10 compatible url definitions

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import os.path
-
 from django.conf import settings
-from django.conf.urls import patterns, include
-from django.views.generic import RedirectView
-from django.core.urlresolvers import reverse
+from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^', include('codespeed.urls')),
-)
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('codespeed.urls')),
+]
 
 if settings.DEBUG:
     # needed for development server

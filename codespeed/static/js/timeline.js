tobami/codespeed

commit 8fb7f1f373c6095cf16e386a12f3accb96acaa64

Display tag on the timeline tooltip

codespeed/static/js/timeline.js
@@ -82,15 +82,15 @@ function getHighlighterConfig(median) {
     return {
       show: true,
       tooltipLocation: 'nw',
-      yvalues: 7,
-      formatString:'<table class="jqplot-highlighter">    <tr><td>date:</td><td>%s</td></tr> <tr><td>median:</td><td>%s</td></tr> <tr><td>max:</td><td>%s</td></tr> <tr><td>Q3:</td><td>%s</td></tr> <tr><td>Q1:</td><td>%s</td></tr> <tr><td>min:</td><td>%s</td></tr> <tr><td>commit:</td><td>%s</td></tr></table>'
+      yvalues: 8,
+      formatString:'<table class="jqplot-highlighter">    <tr><td>date:</td><td>%s</td></tr> <tr><td>median:</td><td>%s</td></tr> <tr><td>max:</td><td>%s</td></tr> <tr><td>Q3:</td><td>%s</td></tr> <tr><td>Q1:</td><td>%s</td></tr> <tr><td>min:</td><td>%s</td></tr> <tr><td>commit:</td><td>%s</td></tr><tr><td>tag:</td><td>%s</td></tr></table>'
     };
   } else {
     return {
       show: true,
       tooltipLocation: 'nw',
-      yvalues: 4,
-      formatString:'<table class="jqplot-highlighter">    <tr><td>date:</td><td>%s</td></tr> <tr><td>result:</td><td>%s</td></tr> <tr><td>std dev:</td><td>%s</td></tr> <tr><td>commit:</td><td>%s</td></tr></table>'
+      yvalues: 5,
+      formatString:'<table class="jqplot-highlighter">    <tr><td>date:</td><td>%s</td></tr> <tr><td>result:</td><td>%s</td></tr> <tr><td>std dev:</td><td>%s</td></tr> <tr><td>commit:</td><td>%s</td></tr><tr><td>tag:</td><td>%s</td></tr></table>'
     };
   }
 }

codespeed/views.py
@@ -306,7 +306,7 @@ def gettimelinedata(request):
                             [
                                 res.revision.date.strftime('%Y/%m/%d %H:%M:%S %z'),
                                 res.value, val_max, q3, q1, val_min,
-                                res.revision.get_short_commitid(), branch
+                                res.revision.get_short_commitid(), res.revision.tag, branch
                             ]
                         )
                     else:
@@ -317,7 +317,7 @@ def gettimelinedata(request):
                             [
                                 res.revision.date.strftime('%Y/%m/%d %H:%M:%S %z'),
                                 res.value, std_dev,
-                                res.revision.get_short_commitid(), branch
+                                res.revision.get_short_commitid(), res.revision.tag, branch
                             ]
                         )
                 timeline['branches'][branch][executable] = results

commit 638f7bc93033eee980b71a33cec404dc299688c2

Use the correct data array pos for commit ID in timeline changes permalinks
Fixes a bug introduced in 8fb7f1f373c6095cf16e386a12f3accb96acaa64.

codespeed/static/js/timeline.js
@@ -69,7 +69,7 @@ function permalinkToChanges(commitid, executableid, environment) {
 function OnMarkerClickHandler(ev, gridpos, datapos, neighbor, plot) {
   if($("input[name='benchmark']:checked").val() === "grid") { return false; }
   if (neighbor) {
-    var commitid = neighbor.data[neighbor.data.length-2];
+    var commitid = neighbor.data[neighbor.data.length-3];
     // Get executable ID from the seriesindex array
     var executableid = seriesindex[neighbor.seriesIndex];
     var environment = $("input[name='environments']:checked").val();
09aa41cd0c11da1cdd8c8ee0804f7f94e6167e28
Ernest W. Durbin III <ewdurbin@gmail.com>
2019-04-08 11:46:27 -0500

    update manage.py settings

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os, sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speed_pypy.settings")
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)

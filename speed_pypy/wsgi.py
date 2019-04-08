e065bbd79952b4c7cc4b2fb49a6cc0242ae6e137
Ernest W. Durbin III <ewdurbin@gmail.com>
2019-04-08 11:35:18 -0500

    Update wsgi.py

--- speed_pypy/wsgi.py
+++ speed_pypy/wsgi.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "speed_pypy.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
